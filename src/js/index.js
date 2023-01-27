032ef46b2f22edff5ef6110c8203c900306dcd61
Erick_Gabriel <123127442+ErickGabriel2023@users.noreply.github.com>
2023-01-26 22:09:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+console.log(video.src);
+const linkDoVideo = video.src;
+
+function alternarModal(){
+   modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+   alternarModal();
+   video.setAttribute("src", linkDoVideo);
+
+}); 
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
+
+
+
+
+
+
+
